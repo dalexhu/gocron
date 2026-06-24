a83d54081151f2d0102c2a0dd71a7063ed2dd5f1
Alex Hu <imalexhu@hotmail.com>
2026-06-23 21:50:16 -0600

    fix: rework top nav with flex layout so menu items no longer overlap and the language switcher is always visible

--- web/vue/src/components/common/navMenu.vue
+++ web/vue/src/components/common/navMenu.vue
@@ -3,75 +3,61 @@
     <el-menu
       :default-active="currentRoute"
       mode="horizontal"
+      :ellipsis="false"
       background-color="#334157"
       text-color="#fff"
       active-text-color="#ffd04b"
       router>
-      <el-row>
-        <el-col style="width: 150px; text-align: center;">
-          <el-menu-item index="/task" style="font-size:16px;letter-spacing:2px;color:rgb(255, 208, 75)">
-          <b @click="changeLogo()">{{logos[logoIndex]}}</b>
-          </el-menu-item>
-        </el-col>
-        <el-col :span="2">
-          <el-menu-item index="/task">
-            <el-icon><Menu/></el-icon>
-            <span>{{ $t('nav.task') }}</span>
-          </el-menu-item>
-        </el-col>
-        <el-col :span="2">
-          <el-menu-item index="/host">
-            <el-icon><Upload/></el-icon>
-            <span>{{ $t('nav.host') }}</span>
-          </el-menu-item>
-        </el-col>
-        <el-col :span="2">
-          <el-menu-item v-if="this.$store.getters.user.isSuperAdmin" index="/user">
-            <el-icon><Service/></el-icon>
-            <span>{{ $t('nav.user') }}</span>
-          </el-menu-item>
-        </el-col>
-        <el-col :span="2">
-          <el-menu-item v-if="this.$store.getters.user.isSuperAdmin" index="/agent/devices">
-            <el-icon><Iphone/></el-icon>
-            <span>{{ $t('nav.agent') }}</span>
-          </el-menu-item>
-        </el-col>
-        <el-col :span="2">
-          <el-menu-item v-if="this.$store.getters.user.isSuperAdmin" index="/system">
-            <el-icon><Setting/></el-icon>
-            <span>{{ $t('nav.system') }}</span>
-          </el-menu-item>
-        </el-col>
-        <el-col style="float:right; text-align:right; width:205px" >
-          <el-sub-menu v-if="this.$store.getters.user.token" index="userStatus">
-            <template #title>
-              <span style="margin-right: 30px; font-size: 16px">{{this.$store.getters.user.username}}</span>
-            </template>
-              <el-menu-item index="/user/edit-my-password">
-                <el-icon><EditPen/></el-icon>
-                <span>{{ $t('nav.changePassword') }}</span>
-              </el-menu-item>
-              <el-menu-item @click="logout" index="/user/logout">
-                <el-icon><CircleClose/></el-icon>
-                <span>{{ $t('nav.logout') }}</span>
-              </el-menu-item>
-          </el-sub-menu>
-        </el-col>
-        <el-col style="float:right; text-align:right; width:120px">
-          <el-dropdown trigger="click" class="lang-switch" @command="switchLanguage">
-            <span class="lang-switch-trigger">
-              {{ currentLangLabel }}<el-icon class="el-icon--right"><ArrowDown/></el-icon>
-            </span>
-            <template #dropdown>
-              <el-dropdown-menu>
-                <el-dropdown-item command="zh-CN" :disabled="$i18n.locale === 'zh-CN'">中文</el-dropdown-item>
-                <el-dropdown-item command="en" :disabled="$i18n.locale === 'en'">English</el-dropdown-item>
-              </el-dropdown-menu>
-            </template>
-          </el-dropdown>
-        </el-col>
-      </el-row>
+      <div class="nav-logo" @click="changeLogo()">{{logos[logoIndex]}}</div>
+      <el-menu-item index="/task">
+        <el-icon><Menu/></el-icon>
+        <span>{{ $t('nav.task') }}</span>
+      </el-menu-item>
+      <el-menu-item index="/host">
+        <el-icon><Upload/></el-icon>
+        <span>{{ $t('nav.host') }}</span>
+      </el-menu-item>
+      <el-menu-item v-if="isSuperAdmin" index="/user">
+        <el-icon><Service/></el-icon>
+        <span>{{ $t('nav.user') }}</span>
+      </el-menu-item>
+      <el-menu-item v-if="isSuperAdmin" index="/agent/devices">
+        <el-icon><Iphone/></el-icon>
+        <span>{{ $t('nav.agent') }}</span>
+      </el-menu-item>
+      <el-menu-item v-if="isSuperAdmin" index="/system">
+        <el-icon><Setting/></el-icon>
+        <span>{{ $t('nav.system') }}</span>
+      </el-menu-item>
+
+      <!-- flex 占位, 把后面的语言切换和用户菜单推到最右 -->
+      <div class="nav-spacer"></div>
+
+      <el-dropdown trigger="click" class="lang-switch" @command="switchLanguage">
+        <span class="lang-switch-trigger">
+          {{ currentLangLabel }}<el-icon class="el-icon--right"><ArrowDown/></el-icon>
+        </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item command="zh-CN" :disabled="$i18n.locale === 'zh-CN'">中文</el-dropdown-item>
+            <el-dropdown-item command="en" :disabled="$i18n.locale === 'en'">English</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+
+      <el-sub-menu v-if="token" index="userStatus">
+        <template #title>
+          <span style="font-size: 16px">{{ username }}</span>
+        </template>
+        <el-menu-item index="/user/edit-my-password">
+          <el-icon><EditPen/></el-icon>
+          <span>{{ $t('nav.changePassword') }}</span>
+        </el-menu-item>
+        <el-menu-item @click="logout" index="/user/logout">
+          <el-icon><CircleClose/></el-icon>
+          <span>{{ $t('nav.logout') }}</span>
+        </el-menu-item>
+      </el-sub-menu>
     </el-menu>
   </div>
 </template>
@@ -88,6 +74,15 @@ export default {
     }
   },
   computed: {
+    isSuperAdmin () {
+      return this.$store.getters.user.isSuperAdmin
+    },
+    token () {
+      return this.$store.getters.user.token
+    },
+    username () {
+      return this.$store.getters.user.username
+    },
     currentRoute () {
       if (this.$route.path === '/') {
         return '/task'
@@ -115,9 +110,28 @@ export default {
 </script>
 
 <style scoped>
-.lang-switch {
+.nav-logo {
+  width: 150px;
+  text-align: center;
   line-height: 60px;
   height: 60px;
+  font-size: 16px;
+  letter-spacing: 2px;
+  font-weight: bold;
+  color: rgb(255, 208, 75);
+  cursor: pointer;
+  user-select: none;
+  flex-shrink: 0;
+}
+.nav-spacer {
+  flex-grow: 1;
+}
+.lang-switch {
+  display: flex;
+  align-items: center;
+  height: 60px;
+  padding: 0 16px;
+  flex-shrink: 0;
 }
 .lang-switch-trigger {
   color: #fff;
